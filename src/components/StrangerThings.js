a2c52972080d488b589652801bb366b646dcd7f0
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-05-06 20:38:00 -0300

    fix: Convertendo as vaŕiáveis de string para number

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -10,12 +10,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 
 const strangerThingsConfig = {
   url: REACT_APP_HAWKINS_URL,
-  timeout: parseFloat(REACT_APP_HAWKINS_TIMEOUT),
+  timeout: REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
   url: REACT_APP_UPSIDEDOWN_URL,
-  timeout: parseFloat(REACT_APP_UPSIDEDOWN_TIMEOUT),
+  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
